59e81f9f9094420179bd017e7ee5542cb955ed81
hamisirizwan <hamisirizwan5@gmail.com>
2024-08-14 19:59:00 +0300

    testing build locally

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,8 @@
 import NextAuth from "next-auth";
-import { ZodError } from "zod";
-import Credentials from "next-auth/providers/credentials";
-import { logInSchema } from "./lib/validationSchemas";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./lib/prisma";
-import bcrypt from "bcryptjs";
-import Google from "next-auth/providers/google";
+
+import authConfig from "./auth.config";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -38,48 +35,5 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       return token;
     },
   },
-  providers: [
-    Credentials({
-      credentials: {
-        email: {},
-        password: {},
-      },
-      authorize: async (credentials) => {
-        try {
-          const { email, password } = await logInSchema.parseAsync(credentials);
-
-          // logic to verify if the user exists
-          let user: any = await prisma.user.findUnique({
-            where: {
-              email,
-            },
-          });
-
-          if (!user || !user.hashedPassword) {
-            return null;
-          }
-
-          const isMatch = bcrypt.compareSync(
-            credentials.password as string,
-            user.hashedPassword
-          );
-
-          if (!isMatch) {
-            return null;
-          }
-
-          return user;
-        } catch (error) {
-          if (error instanceof ZodError) {
-            return null;
-          }
-          return error;
-        }
-      },
-    }),
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+  ...authConfig
 });
