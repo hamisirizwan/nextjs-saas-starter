aa5fc4de805ddf49ac7871549dbca39cb175ac01
hamisirizwan <hamisirizwan5@gmail.com>
2024-08-18 09:22:29 +0300

    added hero sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import LandingPageLayout from "@/components/landingPage/LandingPageLayout";
 import HeroSection from "@/components/landingPage/sections/HeroSection";
+import HeroSection2 from "@/components/landingPage/sections/HeroSection2";
+import HeroSection3 from "@/components/landingPage/sections/HeroSection3";
+import HeroSection4 from "@/components/landingPage/sections/HeroSection4";
 
 export default function Home() {
   return (
   <LandingPageLayout>
    <HeroSection />
+   <HeroSection2 />
+   <HeroSection3 />
+   <HeroSection4 />
   </LandingPageLayout>
   );
 }
--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -11,7 +11,7 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 export const ThemeToggle = () => {
-  const { setTheme } = useTheme();
+  const { setTheme , theme} = useTheme();
 
   return (
     <DropdownMenu>
